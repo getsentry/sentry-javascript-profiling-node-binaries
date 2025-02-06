da6aebf1f6748b3cbe870201c8cea440fcbda875
Tim Fish <tim@timfish.uk>
2025-02-06 13:14:32 +0100

    split tests

--- test/bundler/bundle.mjs
+++ test/bundler/bundle.mjs
@@ -1,14 +1,5 @@
 import { CpuProfilerBindings, ProfileFormat } from '@sentry-internal/node-cpu-profiler';
 
-process.on('uncaughtException', (err) => {
-  console.error(err);
-  process.exit(1);
-});
-process.on('unhandledRejection', (err) => {
-  console.error(err);
-  process.exit(1);
- });
-
 CpuProfilerBindings.startProfiling('test');
 
 setTimeout(() => {
